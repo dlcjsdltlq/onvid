8f8f2904894cf158c8081007694fbc2486d45043
dlcjsdltlq <kumanie1111@naver.com>
2021-06-21 13:55:09 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import MainContainer from './MainContainer';
+import VideoProcessingNotice from './VideoProcessingNotice';
+import { toLibx264 } from './Recorder';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [progressState, setProgress] = useState({
+		isProgress: false,
+		rate: 0,
+	});
+
+	const updateProgress = (frameRate, t, log) => {
+		const { message } = log;
+		if (message.startsWith('frame=')) {
+			setProgress({
+				isProgress: true,
+				rate: (message.split(/\s+/)[1] / (t * frameRate)) * 100,
+			});
+			console.log(message, t);
+		}
+	};
+
+	const downloadBlob = async (blob) => {
+		const url = URL.createObjectURL(blob);
+		const a = document.createElement('a');
+		a.href = url;
+		a.download = new Date().toLocaleString() + '.mp4';
+		a.click();
+	};
+
+	const executeDownload = async (blob, frameRate, t) => {
+		const resultBlob = await toLibx264(blob, (log) =>
+			updateProgress(frameRate, t, log)
+		);
+		setProgress({
+			isProgress: true,
+			rate: 100,
+		});
+		setTimeout(() => {
+			setProgress({ isProgress: false, rate: 100 });
+			setTimeout(() => setProgress({ isProgress: false, rate: 0 }), 400);
+		}, 2500);
+		downloadBlob(resultBlob);
+	};
+
+	return (
+		<div>
+			<MainContainer executeDownload={executeDownload} />
+			<VideoProcessingNotice progressState={progressState} />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,45 @@
+export const recordingTypeList = {
+	1: {
+		screen: {
+			audio: false,
+		},
+	},
+	2: {
+		screen: {
+			audio: true,
+		},
+	},
+	3: {
+		cam: {
+			audio: false,
+		},
+	},
+	4: {
+		cam: {
+			audio: true,
+		},
+	},
+};
+
+export const resolutionOptionList = {
+	1: {
+		width: 640,
+		height: 360,
+	},
+	2: {
+		width: 1280,
+		height: 720,
+	},
+	3: {
+		width: 1920,
+		height: 1080,
+	},
+	4: {
+		width: 2560,
+		height: 1440,
+	},
+	5: {
+		width: 3840,
+		height: 2160,
+	},
+};
